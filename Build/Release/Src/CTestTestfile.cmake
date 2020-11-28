c463bee06bdd1abff9603fee38438c3630f0c26e
trenin17 <61056657+trenin17@users.noreply.github.com>
2020-11-29 00:35:53 +0500

    Deleted temporary files

--- Build/Release/Src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/alextrenin/Documents/GitHub/PathPlanningProject/Src
-# Build directory: /Users/alextrenin/Documents/GitHub/PathPlanningProject/Build/Release/Src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
